ae3622cb81027c5f0bf98017146a6802a1c6a528
Tu Pham <thanhtutdt96@gmail.com>
2022-10-04 11:01:49 +0700

    Update dependencies

--- include/Config.php
+++ include/Config.php
@@ -1,10 +1,21 @@
 <?php
- 
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Looing for .env at the root directory
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 /**
  * Database config variables
  */
-define("DB_HOST", "localhost");
-define("DB_USER", "learneng_admin");
-define("DB_PASSWORD", "admin");
-define("DB_DATABASE", "learneng_learnenglish2017");
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$database = $_ENV['DB_DATABASE'];
+$password = $_ENV['DB_PASSWORD'];
+
+define("DB_HOST", $host);
+define("DB_USER", $user);
+define("DB_PASSWORD", $password);
+define("DB_DATABASE", $database);
 ?>
